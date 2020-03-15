07cb7e16369e23c12f7280a8828f65745da44d68
dennisgon <dennisgon69@hotmail.com>
2020-03-15 15:29:39 +0700

    delete specific array

--- src/Components/Checkout.vue
+++ src/Components/Checkout.vue
@@ -4,15 +4,16 @@
 		<table id="cart" class="table table-hover table-condensed">
 			<thead>
 				<tr>
-					<th style="width:50%">Product</th>
+					<th style="width:30%">Product</th>
 					<th style="width:10%">Price</th>
 					<th style="width:8%">Quantity</th>
 					<th style="width:22%" class="text-center">Subtotal</th>
+					<th style="width:20%" class="text-center">Action</th>
 					<th style="width:10%"></th>
 				</tr>
 			</thead>
 			<tbody>
-				<detail-checkout v-for="cart in getCart" :title="cart.title" :price="cart.price" :qty="cart.qty" ></detail-checkout>
+				<detail-checkout v-for="cart in getCart" :idData="cart.id" :title="cart.title" :price="cart.price" :qty="cart.qty" ></detail-checkout>
 			</tbody>
 		</table>
 		<h2>Total:</h2>
--- src/Components/DetailCheckout.vue
+++ src/Components/DetailCheckout.vue
@@ -12,13 +12,26 @@
 		<td data-th="total">
 		{{qty * price}}
 		</td>
+		<td data-th="action">
+			<button type="button" @click="deleteData(idData)"  class="btn btn-danger">delete</button>
+		</td>
 	</tr> 	
 </template>
 
 <script>
 	export default {
 		name:"checkoutDetail",
-		props:['title','price', 'qty']
+		props:['title','price', 'qty','idData'],
+		methods:{
+			deleteData(id){
+
+				for (var i = 0; i < this.$store.state.cart.length; i++) {
+					if(this.$store.state.cart[i].id == id){
+						this.$store.state.cart.splice(i,1);
+					}
+				}
+			}
+		}
 	}
 
 </script>
